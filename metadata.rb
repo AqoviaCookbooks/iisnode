f9ebc7ee29c06701b2e3573904db4a776f6e399d
Mustafa Arif <mustafa.arif@aqovia.com>
2017-02-16 19:36:03 +0000

    Set copyright notice, declare dependencies and define test platforms.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name 'iisnode'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
+maintainer 'Aqovia'
+maintainer_email 'info@aqovia.com'
+license 'MIT'
 description 'Installs/Configures iisnode'
-long_description 'Installs/Configures iisnode'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+
+depends 'iis_urlrewrite'
+depends 'nodejs'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,4 @@
 # Cookbook Name:: iisnode
 # Recipe:: default
 #
-# Copyright (c) 2017 The Authors, All Rights Reserved.
+# Copyright (c) 2017 Aqovia, All Rights Reserved.
