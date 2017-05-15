e1d8de2452f9fddc50f6b96cdb562f1aac9b2323
Ravindrakumar Solanki <ravindrakumars@interxion.com>
2017-05-15 19:31:13 +0100

    Extracted more attributes and first working unit test

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,23 +4,23 @@
 #
 # Copyright (c) 2017 Aqovia, All Rights Reserved.
 
-if node['iisnode']['iis_flavour'] == 'full' && node['kernel']['machine'] =~ /x86_64/ 
-    package_name = 'iisnode-full-v0.2.21-x64.msi'
-    checksum = '9C65D9E45466B9852D411CF59B039011740A321C02D190AB2128F9F9280BD457'
+if node['iisnode']['iis_edition'] == 'full' && node['kernel']['machine'] =~ /x86_64/
+  package_name = node['iisnode']['server']['installer']['x64']['file']
+  checksum = node['iisnode']['server']['installer']['x64']['checksum']
 
-elsif node['iisnode']['iis_flavour'] == 'full' && node['kernel']['machine'] !~ /x86_64/
-    package_name = 'iisnode-full-v0.2.21-x86.msi'
-    checksum = 'CCFA4FED49CA3A1B744E1318BA1DF44F058FE6D2876E1BE54B3A9F0366BFD41D'
+elsif node['iisnode']['iis_edition'] == 'full' && node['kernel']['machine'] !~ /x86_64/
+  package_name = node['iisnode']['server']['installer']['x86']['file']
+  checksum = node['iisnode']['server']['installer']['x86']['checksum']
 
-elsif node['iisnode']['iis_flavour'] == 'express'
-    package_name = 'iisnode-express-v0.2.21-x86.msi'
-    checksum = '32A45ABB3316662F2E34EBBFFEFB5CDDE66361355A3C35907FD22349C0003E2B'
+elsif node['iisnode']['iis_edition'] == 'express'
+  package_name = node['iisnode']['desktop']['installer']['x86']['file']
+  checksum = node['iisnode']['desktop']['installer']['x86']['checksum']
 
 else
-    raise ("Not able to find matching iisnode package for IIS flavour: " + ['iisnode']['iis_flavour'] + "\nSupported flavours are 'full' and 'express'")
+  raise('Not able to find matching iisnode package for IIS edition: ' + node['iisnode']['iis_edition'] + "\nSupported editions are 'full' and 'express'")
 end
 
-url = 'https://github.com/azure/iisnode/releases/download/v0.2.21/' + package_name
+url = node['iisnode']['store']['url'] + package_name
 
 windows_package 'iisnode' do
   checksum checksum
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,9 +7,9 @@
 require 'spec_helper'
 
 describe 'iisnode::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on Windows Server 2012 R2' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012r2')
       runner.converge(described_recipe)
     end
 
