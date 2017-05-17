373514f6e3063eef3ab3f84e1396713ad47375de
Ravindrakumar Solanki <ravindrakumars@interxion.com>
2017-05-17 11:33:17 +0100

    Added source/issue url for chef supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name 'iisnode'
-maintainer 'Aqovia'
+maintainer 'AqoviaCookbooks'
 maintainer_email 'info@aqovia.com'
+source_url       'https://github.com/AqoviaCookbooks/iisnode'
+issues_url       'https://github.com/AqoviaCookbooks/iisnode/issues'
 license 'MIT'
 description 'Installs/Configures iisnode'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
