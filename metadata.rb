e8dc49eac2fd7a0342cf57dc757c52634b9396c8
Ravindrakumar Solanki <ravindrakumars@interxion.com>
2017-05-22 12:16:21 +0100

    set support only windows and set chef version for build in appveyor

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,9 @@ description 'Installs/Configures iisnode'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.2'
 
+supports 'windows'
+
 depends 'iis_urlrewrite'
 depends 'nodejs'
+
+chef_version '>= 12.6' if respond_to?(:chef_version)
